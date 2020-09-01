587b98397f264e6ae95d668041213a23d4c89466
onion013 <68872474+onion013@users.noreply.github.com>
2020-09-01 11:00:14 +0200

    db init und connection functiond
    still needs table names and so on

--- src/model/DatabaseFunctions.java
+++ src/model/DatabaseFunctions.java
@@ -1,9 +1,37 @@
 package model;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseFunctions {
 	private static Connection con;
 	private static boolean hasData = false;
 	
+	private void getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("org.sqlite.JDBC");
+		con = DriverManager.getConnection("jdbc:sqlite:ServerData.db");
+		//DatabaseData.db is the name of the file if changed has to change here to
+		// file doesn't exist currently
+		
+		initialise();
+	}
+	
+	private void initialise() throws SQLException {
+		if(!hasData) 
+			hasData = true;
+		Statement state = con.createStatement();
+		ResultSet result = state.executeQuery("SELECT name FROM sqlite_master WHERE type='table' and name = ''");
+		//todo: Name of table has to be added
+		
+		if(!result.next()) {
+			System.out.println("Building the necessary tables");
+			Statement buildState = con.createStatement();
+			buildState.execute("CREATE TABLE x()");
+			//todo add tables that need to be created
+		}
+		
+	}
 }
