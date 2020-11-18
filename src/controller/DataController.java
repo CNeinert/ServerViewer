148a5ea1944fb84f9f702f0469b558807c5b1e9a
onion013 <68872474+onion013@users.noreply.github.com>
2020-11-18 10:19:06 +0100

    static block, SaveServer Method

--- src/controller/DataController.java
+++ src/controller/DataController.java
@@ -1,5 +1,29 @@
 package controller;
 
-public class DataController {
+import java.sql.SQLException;
 
+import model.Database;
+import model.Server;
+
+public class DataController {
+	
+	private static Database db;
+	
+	static {
+		try {
+			db = new Database();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void SaveServer(Server server) {
+		try {
+			db.insertServer(server);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 }
