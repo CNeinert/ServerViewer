4417724c12b42d7430c008757dc70104fc65ff8b
CNeinert <c.neinert@t-online.de>
2020-12-21 18:52:18 +0100

    [controller] added controller for the root view

---
+++ src/controller/V_ServerViewerController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import javafx.scene.control.Button;
+
+public class V_ServerViewerController {
+	
+	Button SettingsButton = new Button();
+	
+	public void showSettings(){
+		
+	}
+}
